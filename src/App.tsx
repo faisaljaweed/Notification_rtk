e0bdd414907e519770f8f4de1046d9e110e1960f
faisaljaweed <faisaljawed2000@gmail.com>
2024-09-11 09:42:46 -0700

    Error REsolves

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Header } from './components/Header'
-import { Notification } from './components/Notification'
+import "./App.css";
+import { Header } from "./components/Header";
+import { Notification } from "./components/Notification";
 
 function App() {
-
-
   return (
     <>
-     <Header/>
-     <Notification/>
+      <Header />
+      <Notification />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
